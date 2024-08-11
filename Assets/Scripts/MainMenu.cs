e2a707bef0a7397e08e17cd40c8df2d4c2cf625c
nkplka <nkplkaa@gmail.com>
2024-08-11 09:17:09 +0300

    я облачко

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,34 +4,59 @@ using UnityEngine.UI;
 public class VolumeControl : MonoBehaviour
 {
     public Slider volumeSlider;
-    public AudioSource audioSource; 
+    public AudioSource audioSource;
+    public AudioClip[] audioClips; 
+    public Button resetButton; // Добавьте ссылку на кнопку Reset
 
     void Start()
     {
-        
         if (audioSource != null)
         {
-            
-            audioSource.volume = 0.2f; 
-
-            
+            audioSource.volume = 0.1f; 
             volumeSlider.value = audioSource.volume;
+
+            volumeSlider.onValueChanged.AddListener(SetVolume);
+            PlayRandomClip();
+
+            if (resetButton != null)
+            {
+                resetButton.onClick.AddListener(OnResetButtonClicked);
+            }
+            else
+            {
+                Debug.LogError("ResetButton is not assigned.");
+            }
         }
         else
         {
             Debug.LogError("AudioSource is not assigned.");
         }
-
-        
-        volumeSlider.onValueChanged.AddListener(SetVolume);
     }
 
     void SetVolume(float volume)
     {
-        
         if (audioSource != null)
         {
             audioSource.volume = volume;
         }
+    }
+
+    public void PlayRandomClip()
+    {
+        if (audioClips.Length > 0)
+        {
+            int randomIndex = Random.Range(0, audioClips.Length);
+            audioSource.clip = audioClips[randomIndex];
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("No audio clips assigned.");
+        }
+    }
+
+    void OnResetButtonClicked()
+    {
+        PlayRandomClip(); // Воспроизводим новый случайный трек
     }
 }
