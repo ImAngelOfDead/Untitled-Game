8c4544597cc4fa3a1e0f1e23fadd1a6bdf5aac26
nkplka <nkplkaa@gmail.com>
2024-08-12 22:35:00 +0300

    стим залупа

--- Assets/Scripts/PlayerSaver.cs
+++ Assets/Scripts/PlayerSaver.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Saver : MonoBehaviour
 {
     public float teleportThreshold = -10f;
-    public Vector3 teleportPosition = new Vector3(-10f, 2f, -10f);
+    public Vector3 teleportPosition = new Vector3(-140f, 2f, -10f);
 
     void Update()
     {
---
+++ Assets/Scripts/SteamManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamManager : MonoBehaviour
+{
+    private static SteamManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        try
+        {
+            Steamworks.SteamClient.Init(480);
+            Debug.Log(Steamworks.SteamClient.IsValid);
+            var mySteamId = Steamworks.SteamClient.SteamId;
+            Debug.Log($"SteamId: {mySteamId}");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Error: {e.Message}");
+        }
+    }
+
+    void Update()
+    {
+        Steamworks.SteamClient.RunCallbacks();
+    }
+}
